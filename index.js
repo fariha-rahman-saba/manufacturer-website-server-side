9fc15265a00716c709ede1a38a57eb80b28e19cd
saba rahman <sabarahman.official@gmail.com>
2022-05-24 01:27:38 +0600

    database connected

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yhyer.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run () {
+    try {
+        await client.connect();
+        console.log("database connected");
+        const toolCollection = client.db('tools-manufacturer').collection('tools');
+        const userCollection = client.db('tools-manufacturer').collection('users');
+        const orderCollection = client.db('tools-manufacturer').collection('orders');
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello From Tools Manufacturer');
+});
+
+app.listen(port, () => {
+    console.log(`Tools Manufacturer listening on port ${port}`);
+});
