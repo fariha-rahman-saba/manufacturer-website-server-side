0f30aa6177c10b1d9bb5485ad8e74af271f6cb33
saba rahman <sabarahman.official@gmail.com>
2022-05-24 02:24:36 +0600

    readme added

---
+++ README.md
@@ -0,0 +1,18 @@
+# Tool manufacturer Website
+### live website link:
+#### This is the code server of the server side of our Tool manufacturer website
+***
+## Technologies:
+A list of technologies used within the project:
+* [node.js]
+* [express.js]
+* [mongodb]
+* [Heroku]
+***
+## Details:
+* In this project I have created an online Tool manufacturer website
+* In Home page I have added header, banner, some tools, about us section, reviews and footer section
+* In Blogs I have answered some questions
+* I have also created a 404 route
+* I have created private routes which we cannot access without login
+* I have implemented email and password authentication and google authentication
--- index.js
+++ index.js
@@ -20,6 +20,25 @@ async function run () {
         const toolCollection = client.db('tools-manufacturer').collection('tools');
         const userCollection = client.db('tools-manufacturer').collection('users');
         const orderCollection = client.db('tools-manufacturer').collection('orders');
+
+        app.get('/user', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            // res.send({ result, token });
+            res.send(result);
+        });
     }
     finally {
 
