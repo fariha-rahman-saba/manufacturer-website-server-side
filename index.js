7810dd0ecc1833baa89808148ffaed71bee2656b
saba rahman <sabarahman.official@gmail.com>
2022-05-25 10:31:00 +0600

    add a review working

--- index.js
+++ index.js
@@ -20,24 +20,49 @@ async function run () {
         const toolCollection = client.db('tools-manufacturer').collection('tools');
         const userCollection = client.db('tools-manufacturer').collection('users');
         const orderCollection = client.db('tools-manufacturer').collection('orders');
+        const reviewCollection = client.db('tools-manufacturer').collection('reviews');
 
-        app.get('/user', async (req, res) => {
-            const users = await userCollection.find().toArray();
-            res.send(users);
+        // app.get('/user', async (req, res) => {
+        //     const users = await userCollection.find().toArray();
+        //     res.send(users);
+        // });
+
+        // app.put('/user/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     const user = req.body;
+        //     const filter = { email: email };
+        //     const options = { upsert: true };
+        //     const updateDoc = {
+        //         $set: user,
+        //     };
+        //     const result = await userCollection.updateOne(filter, updateDoc, options);
+        //     // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+        //     // res.send({ result, token });
+        //     res.send(result);
+        // });
+
+        // Get requests 
+        app.get('/tool', async (req, res) => {
+            const tools = await toolCollection.find().toArray();
+            res.send(tools);
+        });
+        app.get('/review', async (req, res) => {
+            const reviews = await reviewCollection.find().toArray();
+            res.send(reviews);
+        });
+        app.get("/purchase/:id", async (req, res) => {
+            const toolId = req.params.id;
+            const query = { _id: ObjectId(toolId) };
+            const tool = await toolCollection.findOne(query);
+            res.send(tool);
         });
 
-        app.put('/user/:email', async (req, res) => {
-            const email = req.params.email;
-            const user = req.body;
-            const filter = { email: email };
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: user,
-            };
-            const result = await userCollection.updateOne(filter, updateDoc, options);
-            // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            // res.send({ result, token });
-            res.send(result);
+        // post request
+        app.post("/review", async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            console.log("Review added");
+            res.send({ success: 'product added' });
         });
     }
     finally {
